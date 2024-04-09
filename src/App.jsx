4378c1823e76713a2abfc612b6b801fb4c8cce3a
bkvats <bkvats2394@gmail.com>
2024-04-09 21:08:17 +0530

    adding values parameter

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import MainBox from "./components/MainBox.jsx"
 function App() {
-  const [currencyValueOne, setCurrencyValueOne] = useState(1);
-  const [currencyValueTwo, setCurrencyValueTwo] = useState(82.22);
   return (
     <>
     <div className="playground">
-      <MainBox desg="from" currency="USD" currencyValueOne={currencyValueOne} setCurrencyValueOne={setCurrencyValueOne} currencyValueTwo={currencyValueTwo} setCurrencyValueTwo={setCurrencyValueTwo} />
-      <MainBox desg="to" currency="IND" currencyValueOne={currencyValueTwo} setCurrencyValueOne={setCurrencyValueTwo} currencyValueTwo={currencyValueTwo} setCurrencyValueTwo={setCurrencyValueTwo} />
+      <MainBox desg="from" currencyname="usd" />
+      <MainBox desg="to" currencyname="inr" />
     </div>
     </>
   )
--- src/components/MainBox.jsx
+++ src/components/MainBox.jsx
@@ -1,24 +1,40 @@
+import { useState } from "react";
 import getRate  from "./getRate";
-function MainBox({desg, currencyOne, currencyTwo, currencyValueOne, setCurrencyValueOne, currencyValueTwo, setCurrencyValueTwo}) {
+function MainBox({desg, currencyname, readOnly = false, resultValue = null, setResultValue = null}) {
+    const [otherCurrencyValue, setOtherCurrencyValue] = useState(1);
+    const [currency, setCurrency] = useState(currencyname);
+    const [currencyList, setCurrencyList] = useState([currency]);
+    async function getCurrencyList() {
+        let p = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`);
+        let data = await p.json();
+        var currencyMap = data[currency];
+        setCurrencyList(Object.keys(currencyMap));
+    }
     return (
         <div className="mainBox">
-            <div className="upperContent">
-                <p>{desg}</p>
-                <h1>{currency}</h1>
-                <div className="circle"></div>
-            </div>
-            <input type="text" 
-            id="value"
-            value={currencyValueOne}
-            onChange={async (event) => {
+            <p>{desg}</p>
+            <h1>{currency.toUpperCase()}</h1>
+            <label htmlFor="currencySelect">Select a Currency:</label>
+            <select id="currencySelect" onClick={getCurrencyList} onChange={(event) => {
+                setCurrency(event.target.value);
+                otherCurrencyValue(currencyMap[event.target.value])
+                }} value={currency}>
+                {
+                currencyList.map((value, index) => (
+                        <option key={index} value={value}>{value}</option>
+                    ))
+                }
+            </select>
+            <input
+            type="text"
+            readOnly={readOnly}
+            onChange={(event) => {
                 let value = Number(event.target.value);
                 if (!isNaN(value)) {
-                    setCurrencyValueOne(event.target.value)
-                    console.log(await getRate(currencyOne.toLowerCase(), currencyTwo.toLowerCase()));
+
                 }
             }}
-            />
-            <div className="line"></div>
+            ></input>
         </div>
     )
 }
